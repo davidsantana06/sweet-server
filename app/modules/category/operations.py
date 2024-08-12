fb4cc4e663c0e748f50588800963ee22557cfe25
David Santana <davidsantana06@hotmail.com>
2024-08-12 17:09:01 -0300

    feat(category): add get_one_by_id operation

--- app/modules/category/operations.py
+++ app/modules/category/operations.py
@@ -13,8 +13,19 @@ def get_all_select_choices() -> SelectChoices:
     return Category.find_all_select_choices()
 
 
-def get_one_by_name(name: str) -> Category:
-    category = Category.find_first_by_name(name)
+def _check_existance(category: Category) -> bool:
     if not category:
         raise NotFound('The category was not found.')
+    return True
+
+
+def get_one_by_id(id: int) -> Category:
+    category = Category.find_first_by_id(id)
+    _ = _check_existance(category)
+    return category
+
+
+def get_one_by_name(name: str) -> Category:
+    category = Category.find_first_by_name(name)
+    _ = _check_existance(category)
     return category
