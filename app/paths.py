8f56f355f08e099677e3bd96544bb67b4ca6ccd5
David Santana <davidsantana06@hotmail.com>
2024-09-11 16:41:03 -0300

    refactor: remove data folder

--- app/paths.py
+++ app/paths.py
@@ -6,6 +6,5 @@ ENV_FILE = path.join(_ROOT_FOLDER, '.env')
 
 _STORAGE_FOLDER = path.join(_ROOT_FOLDER, 'storage')
 
-_DATA_FOLDER = path.join(_STORAGE_FOLDER, 'data')
-SETUP_FILE = path.join(_DATA_FOLDER, 'setup.json')
-DATABASE_FILE = path.join(_DATA_FOLDER, 'database.sqlite3')
+SETUP_FILE = path.join(_STORAGE_FOLDER, 'setup.json')
+DATABASE_FILE = path.join(_STORAGE_FOLDER, 'database.sqlite3')
