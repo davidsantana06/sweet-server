d1b8beda51d3fc328b88ef50e3576b55b069a023
David Santana <davidsantana06@hotmail.com>
2024-10-03 16:17:10 -0300

    refactor(common): use flask in response facade

--- app/modules/common/facades/response.py
+++ app/modules/common/facades/response.py
@@ -1,5 +1,6 @@
+from flask import Response, jsonify, make_response
 from http import HTTPStatus
-from typing import Dict, List, Tuple, Union
+from typing import Dict
 
 from app.database import (
     Model, Models,
@@ -7,50 +8,59 @@ from app.database import (
 )
 
 
-_Response = Tuple[
-    Union[
-        List[Dict[str, object]],
-        Dict[str, object]
-    ],
-    HTTPStatus
-]
-
-
 class ResponseFacade():
     def as_dict(
         self,
-        dict: Dict,
+        data: Dict,
         status: HTTPStatus = HTTPStatus.OK
-    ) -> _Response:
-        return dict, status
+    ) -> Response:
+        return self.__jsonify(data, status)
 
     def as_message(
         self,
         message: str,
         status: HTTPStatus = HTTPStatus.OK
-    ) -> _Response:
-        return {'message': message}, status
+    ) -> Response:
+        return self.__jsonify(
+            {'message': message},
+            status
+        )
 
     def as_model(
         self,
         model: Model,
         status: HTTPStatus = HTTPStatus.OK
-    ) -> _Response:
-        return model.to_dict(), status
+    ) -> Response:
+        return self.__jsonify(model.to_dict(), status)
 
     def as_models(
         self,
         models: Models,
         status: HTTPStatus = HTTPStatus.OK
-    ) -> _Response:
-        return [model.to_dict() for model in models], status
+    ) -> Response:
+        return self.__jsonify(
+            [model.to_dict() for model in models],
+            status
+        )
 
     def as_select_choices(
         self,
         select_choices: SelectChoices,
         status: HTTPStatus = HTTPStatus.OK
-    ) -> _Response:
-        return [
-            {'value': value, 'label': label}
-            for value, label in select_choices
-        ], status
+    ) -> Response:
+        return self.__jsonify(
+            [
+                {'value': value, 'label': label}
+                for value, label in select_choices
+            ], status
+        )
+
+    def __jsonify(
+        self,
+        data: object,
+        status: HTTPStatus = HTTPStatus.OK
+    ) -> Response:
+        return make_response(
+            jsonify(data),
+            status
+        )
