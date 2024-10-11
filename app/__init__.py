20ac2768090c2784a000841278bd7cb0be6bd1f3
David Santana <davidsantana06@hotmail.com>
2024-10-10 22:10:09 -0300

    feat(common): add base form

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 from .config import configure_enviroment, configure_extensions
+from .modules.common import common
 
 
 app = Flask(__name__)
 configure_enviroment(app)
 configure_extensions(app)
+app.register_blueprint(common)
