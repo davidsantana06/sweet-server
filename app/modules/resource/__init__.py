f981edf32e6c693304a50ae53202d9b4af4250e8
David Santana <davidsantana06@hotmail.com>
2024-10-11 11:25:11 -0300

    fix(resource): register material blueprint

--- app/modules/resource/__init__.py
+++ app/modules/resource/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 
 from .modules.ingredient import ingredient
+from .modules.material import material
 
 
 resource = Blueprint('resource', __name__)
 resource.register_blueprint(ingredient)
+resource.register_blueprint(material)
