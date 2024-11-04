61b6e7881e3bde899c7cf40098eb42053c159d4f
David Santana <davidsantana06@hotmail.com>
2024-11-04 11:31:48 -0300

    refactor: segment setup data

--- app/config/path.py
+++ app/config/path.py
@@ -10,8 +10,20 @@ ENV_FILE = ROOT_DIR / '.env'
 STORAGE_DIR = ROOT_DIR / 'storage'
 ''' /storage/ '''
 
-SETUP_FILE = STORAGE_DIR / 'setup.json'
-''' /storage/setup.json '''
-
 DATABASE_FILE = STORAGE_DIR / 'database.sqlite3'
 ''' /storage/database.sqlite3 '''
+
+SETUP_DIR = STORAGE_DIR / 'setup'
+''' /storage/setup/ '''
+
+DEFAULT_CATEGORIES_DATA_FILE = SETUP_DIR / 'default_categories.json'
+'''  /storage/setup/default_categories.json '''
+
+DEFAULT_COLLABORATOR_DATA_FILE = SETUP_DIR / 'default_collaborator.json'
+'''  /storage/setup/default_collaborator.json '''
+
+DEFAULT_PAYMENT_METHODS_DATA_FILE = SETUP_DIR / 'default_payment_methods.json'
+'''  /storage/setup/default_payment_methods.json '''
+
+USER_DATA_FILE = SETUP_DIR / 'user.json'
+'''  /storage/setup/user.json '''
--- app/config/setup.py
+++ app/config/setup.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask
+from pathlib import Path
 from typing import List
 import json
 
@@ -15,13 +16,18 @@ from app.resource import (
     payment_method_ns,
     user_ns
 )
+from app.schema import (
+    CategorySchema,
+    CollaboratorSchema,
+    PaymentMethodSchema,
+    UserSchema
+)
 from app.service import (
     category_service,
     collaborator_service,
     payment_method_service,
     user_service
 )
-from app.typing import DefaultCollaborator, SetupData, User
 
 from . import parameter
 from . import path
@@ -68,37 +74,44 @@ def setup_extensions(app: Flask) -> None:
     _setup_cors(app)
 
 
-def _get_setup_data() -> SetupData:
-    with open(path.SETUP_FILE, encoding='utf-8') as file:
+def _get_setup_data(file_path: Path) -> object:
+    with open(file_path, encoding='utf-8') as file:
         return json.load(file)
 
 
-def _create_default_categories(names: List[str]) -> None:
-    for name in names:
-        try: category_service.get_one_by('name', name)
-        except: category_service.create({'name': name})
+def _create_default_categories(data: List[CategorySchema]) -> None:
+    for category in data:
+        try: category_service.get_one_by(category['name'])
+        except: category_service.create(category)
 
 
-def _create_default_collaborator(data: DefaultCollaborator) -> None:
+def _create_default_collaborator(data: CollaboratorSchema) -> None:
     try: collaborator_service.get_one_by_id(1, except_default=False)
     except: collaborator_service.create(data)
 
 
-def _create_default_payment_methods(names: List[str]) -> None:
-    for name in names:
-        try: payment_method_service.get_one_by('name', name)
-        except: payment_method_service.create({'name': name})
+def _create_default_payment_methods(data: List[PaymentMethodSchema]) -> None:
+    for payment_method in data:
+        try: payment_method_service.get_one_by(payment_method['name'])
+        except: payment_method_service.create(payment_method)
 
 
-def _create_user(data: User) -> None:
+def _create_user(data: UserSchema) -> None:
     try: user_service.get_one()
     except: user_service.create(data)
 
 
 def setup_entities(app: Flask) -> None:
-    data = _get_setup_data()
     with app.app_context():
-        _create_default_categories(data['default_categories'])
-        _create_default_collaborator(data['default_collaborator'])
-        _create_default_payment_methods(data['default_payment_methods'])
-        _create_user(data['user'])
+        _create_default_categories(
+            _get_setup_data(path.DEFAULT_CATEGORIES_DATA_FILE)
+        )
+        _create_default_collaborator(
+            _get_setup_data(path.DEFAULT_COLLABORATOR_DATA_FILE)
+        )
+        _create_default_payment_methods(
+            _get_setup_data(path.DEFAULT_PAYMENT_METHODS_DATA_FILE)
+        )
+        _create_user(
+            _get_setup_data(path.USER_DATA_FILE)
+        )
